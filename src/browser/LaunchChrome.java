87a108cd1c0d50e97cbc620cdd4970864ccd6024
Shahika6774 <64507734+Shahika6774@users.noreply.github.com>
2020-05-19 23:51:06 -0400

    first comit

---
+++ src/browser/LaunchChrome.java
@@ -0,0 +1,23 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SHAHIKA\\eclipse-newworkspace\\LaunchBrowser\\Driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://techfios.com/test/billing/?ng=admin/");
+		
+		//driver.close();
+			
+	}
+	
+	// http://techfios.com/test/billing/?ng=admin/
+	
+	//http:
+
+}
